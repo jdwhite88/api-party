bbd07dedee4a5f7f94c4b52bf1ff47c3ce294087
jdwhite88 <white570@purdue.edu>
2018-05-29 15:54:11 -0400

    Fetch user data

--- src/GitHubUser.js
+++ src/GitHubUser.js
@@ -1,11 +1,43 @@
 import React, {Component } from 'react';
 
+import './GitHubUser.css';
+
 class GitHubUser extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: {},
+        };
+
+        this.fetchUserData(props);
+    }
+
+    fetchUserData = (props) => {
+        fetch(`https://api.github.com/users/${props.match.params.username}`)
+            .then( response => response.json())
+            .then( user => this.setState( {user} ));
+    }
+
+    componentWillReceiveProps = (newProps) => {
+        const locationChanged = newProps.location !== this.props.location;
+        if (locationChanged) {
+            this.fetchUserData(newProps);
+        }
+    }
+
     render() {
+
+        const { user } = this.state;
         return (
-            <div className="GithubUser">
-                <h1>GitHub user: {this.props.match.params.username}</h1>
+            <div className="GitHubUser">
+                <img src={user.avatar_url} alt="" />
+                <h2>{user.login}</h2>
+                <h3>following: {user.following}</h3>
+                <h3>followers: {user.followers}</h3>
+                <h3>location: {user.location}</h3>
+                <a href={user.html_url} target="_blank">Link to {user.login}'s profile</a>
             </div>
+
         );
     }
 }
